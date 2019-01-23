eddc7b647ed20faf2d76ec2e8cc5eb87d437d673
dmerniestic1987 <dmernies@uade.edu.ar>
2019-01-23 16:32:05 -0300

    Se sube ejemplo 2

---
+++ react/ejemplo02/src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './components/Navigation';
+import { tickers } from './tickers.json';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tickers
+    }
+  }
+  render() {
+    const tickerList = this.state.tickers.map((ticker) => {
+      
+      return (<div className="card"> 
+                <div className="card-header">
+                  {ticker.tickerSymbol}
+                </div>
+                {ticker.tickerName}
+              </div> ); 
+    });
+
+    return (
+      <div className="App">
+        <Navigation titulo="Manteca"/>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+            Learn React
+        </header>
+
+        { tickerList }
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ react/ejemplo02/src/components/Navigation.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class Navigation extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+          operaciones: []
+        }    
+      }
+    handleClick(buttonName){
+        const operaciones = this.state.operaciones.concat([buttonName]);
+        this.setState({
+          operaciones : operaciones
+        });
+        alert('Usted hizo click en: ' + buttonName);
+    }
+      
+    render() {
+        return (
+            <nav className="navbar navbar-default navbar-static-top">
+            <h1 >{this.props.titulo.toUpperCase()}</h1>
+            <button className="btn btn-lg btn-primary bg-dark" 
+                    onClick={() => {this.handleClick('Inicio')}}>Inicio</button>
+            <button className="btn btn-lg btn-primary" 
+                    onClick={() => {this.handleClick('Fin')}}>Fin</button>
+            </nav>
+        );
+    }
+}
+
+export default Navigation;
