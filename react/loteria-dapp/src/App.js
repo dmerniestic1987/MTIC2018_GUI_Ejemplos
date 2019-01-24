93d1d6aa4b673cfebe5de29702ae0fd943c0d67b
dmerniestic1987 <dmernies@uade.edu.ar>
2019-01-24 18:16:36 -0300

    se agrega interacción con blockchain

--- react/loteria-dapp/src/App.js
+++ react/loteria-dapp/src/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo_loteria.svg';
 import './App.css';
 import web3 from './web3'
 import loteria from './loteria'
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      jugadores: [],
+      balanceAcumulado: 0,
+      ultimoGanador: 'x0',
+      loteriaFinalizada: false, 
+      huboError: false,
+      mensajeError: '', 
+      valorApostado: '', 
+      cuenta: ''      
+    }
+  }
   async componentDidMount() {
     const jugadores = await loteria.methods.getJugadores().call();
     console.log(jugadores);
@@ -12,38 +25,96 @@ class App extends Component {
     console.log(balance);
     const ultimoGanador = await loteria.methods.getUltimoGanador().call();
     console.log(ultimoGanador);
-
+    const cuenta = await web3.eth.getAccounts();
     this.setState({
       jugadores: jugadores,
       balanceAcumulado: balance,
-      ultimoGanador: ultimoGanador
+      ultimoGanador: ultimoGanador,
+      loteriaFinalizada: false, 
+      huboError: false,
+      mensajeError: '',
+      valorApostado: '',
+      cuenta: cuenta
     });
   }
 
-  render() {
-    web3.eth.getAccounts().then(console.log);
-    
-    
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+  async seleccionarGanador(){
+    try{
+      await loteria.methods.elegirGanador().call();
+    } catch(err) {
+      this.setState({
+        huboError: true, 
+        mensajeError: err
+      });
+    }
+  }
 
-        <div className="bg-dark">
-          HOlanp
+  async handleClick(){
+    try {
+      alert('Apostar');
+      await loteria.methods.jugar().send( {
+                                            from: this.state.cuenta[0], 
+                                            value: web3.utils.toWei(this.state.valorApostado, 'ether')
+                                          });
+    } catch(err) {
+      this.setState({
+        huboError: true, 
+        mensajeError: err
+      });
+      alert(err);
+    }
+  }
+  
+  handleChange = (name, event) => {
+    this.setState(
+        {[name]: event.target.value}
+    );
+  }
+
+  render() {   
+    return (
+      <div className="App bg-dark">
+        <div className="container mt-4">
+          <div className="card">
+            <div className="card-header">
+              Lotería Loca! Rinkeby Ethereum
+            </div>
+            <div className="card-body">
+              <h5 className="card-title">Juego del día: {new Date().toLocaleDateString()} </h5>
+                <img src={logo} className="App-logo mt-4 mb-4" alt="logo" /> <br />
+                <p className="card-text mt-2">
+                  Balance Acumulado: {this.state.balanceAcumulado + ' WEI'} <br />
+                  Último Ganador: {this.state.ultimoGanador} <br />
+                  Lotería Finalizada: {this.state.loteriaFinalizada}
+                </p>
+            </div>
+          </div>
         </div>
+
+        <div className="container mt-4">
+          <div className="card">
+            <h5 className="card-title">Jugar a la Lotería </h5>
+            <form className="card-body">
+            <div className="form-group">
+              <input  type ="number"
+                      onChange={(event) => this.handleChange('valorApostado', event)}
+                      value={this.state.valorApostado}
+                      className="form-control"
+                      placeholder="Precio" />
+              ETH
+            </div>
+            <div className="form-group">
+              <input  type ="button"
+                      name="guardar"
+                      onClick={() => this.handleClick()}
+                      className="form-control btn btn-primary"
+                      placeholder="Precio"
+                      value="Guardar" 
+                      step=".001"/>
+            </div>                    
+            </form>
+          </div>
+        </div>        
       </div>
     );
   }
