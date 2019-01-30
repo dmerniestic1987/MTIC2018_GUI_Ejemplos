13ed92a0c9c28c458e6506600d8c9171a83f5008
dmerniestic1987 <dmernies@uade.edu.ar>
2019-01-30 10:58:30 -0300

    Se agrega nuevo proyecto

---
+++ react/next-crowfounding-dapp/client/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default() => {
+    return(
+        <div>
+            <h1>Esta es la lista de Proyectos</h1>
+            <h2>Espero que ande... gato</h2>
+        </div>
+    );
+}
