e81879245810a17d2a447262a8d226375ce167fc
dmerniestic1987 <dmernies@uade.edu.ar>
2019-02-01 22:04:48 -0300

    Se corrige página para consultar total de inversores

--- react/crowfounding-next/client/lib/Web3Container.js
+++ react/crowfounding-next/client/lib/Web3Container.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import getWeb3 from './getWeb3'
-import contractDefinition from './contracts/ProyectoInversion.json'
+import getContract from './getContract'
+import contractDefinition from './contracts/FactoryProyectoInversion.json'
 
-export default class ProyectoWeb3Container extends React.Component {
-  state = { web3: null, accounts: null, contractDefinition: null };
+export default class FactoryWeb3Container extends React.Component {
+  state = { web3: null, accounts: null, contract: null };
 
   async componentDidMount () {
     try {
       const web3 = await getWeb3()
       const accounts = await web3.eth.getAccounts()
-      this.setState({ web3, accounts, contractDefinition })
+      const contract = await getContract(web3, contractDefinition)
+      this.setState({ web3, accounts, contract })
     } catch (error) {
       alert(
         'No se pudo cargar una instancia de web3, o las cuentas o el contrato.'
@@ -19,9 +21,9 @@ export default class ProyectoWeb3Container extends React.Component {
   }
 
   render () {
-    const { web3, accounts, contractDefinition } = this.state
+    const { web3, accounts, contract } = this.state
     return web3 && accounts
-      ? this.props.render({ web3, accounts, contractDefinition })
+      ? this.props.render({ web3, accounts, contract })
       : this.props.renderLoading()
   }
 }
--- react/crowfounding-next/client/pages/proyectos/index.js
+++ react/crowfounding-next/client/pages/proyectos/index.js
@@ -1,21 +1,103 @@
-import React from 'react'
+import React, {Component} from 'react'
 import ProyectoWeb3Container from '../../lib/ProyectoWeb3Container'
 import {withRouter} from 'next/router'
 import Layout from '../../components/Layaout'
 
 const Proyecto = withRouter((props) => (
     <Layout>
-       <h1>{props.router.query.address}</h1>
-       <p>{props.router.query.texto}</p>
+       <ProyectoShow address = {props.router.query.address} 
+                     accounts = {props.accounts}
+                     web3 = {props.web3}
+                     contractDefinition = {props.contractDefinition}/>
     </Layout>
 ))
 
+class ProyectoShow extends Component{
+  state={
+    web3: null,
+    contractDefinition: null,
+    address: null,
+    accounts: null, 
+    contractInstance: null,
+    totalRequerido: undefined,
+    contribucionMinima: undefined, 
+    cantidadInversores: undefined
+  }
 
-export default () => (
+  async componentDidMount () {
+    try {
+      await this.getTotalRequerido();
+      await this.getContribucionMinima();
+      await this.getCantidadInversores();
+
+    } catch (error) {
+      alert(
+        'No se pudo cargar una instancia de web3, o las cuentas o el contrato. '
+        .concat(error.message + '. ')
+        .concat('Revise la consola para más detalle'));
+      console.log(error)
+    }
+  }
+
+  getContractInstance() {
+    const web3 = this.props.web3;
+    const contractDefinition = this.props.contractDefinition;
+    const contractInstance = new web3.eth.Contract(
+      contractDefinition.abi,
+      this.props.address
+    ) 
+    return contractInstance;   
+  }
+
+  getTotalRequerido = async() => {    
+    const instance = this.getContractInstance();
+    const web3 = this.props.web3;
+    const totalRequeridoWei = await instance.methods.totalRequerido().call();
+    const totalRequeridoEther = web3.utils.fromWei(totalRequeridoWei, 'ether');
+    this.setState({totalRequerido: totalRequeridoEther});
+
+          /*
+    uint public cantidadInversores;
+      */
+
+  }
+
+  getContribucionMinima = async() =>{
+    const instance = this.getContractInstance();
+    const web3 = this.props.web3;
+    const contribucionMinimaWei = await instance.methods.contribucionMinima().call();
+    const contribucionMinimaEther = web3.utils.fromWei(contribucionMinimaWei, 'ether');
+    this.setState({contribucionMinima: contribucionMinimaEther});
+  }
+
+  async getCantidadInversores(){
+    const instance = this.getContractInstance();
+    const cantidadInversores = await instance.methods.cantidadInversores().call();
+    this.setState({cantidadInversores: cantidadInversores});
+  }
+
+  render(){
+    const addressContract = this.props.address;
+    const { totalRequerido = 'N/A'
+          , contribucionMinima = 'N/A'
+          , cantidadInversores = 'N/A' } = this.state;
+
+    return (
+      <div>
+        <h1>{addressContract}</h1>
+        Total Requerido: {totalRequerido} ETH <br />
+        Contribución Mínima: {contribucionMinima} ETH <br />
+        Cantidad de Inversores: {cantidadInversores}
+      </div>
+    )
+  }
+}
+
+export default (props) => (
     <ProyectoWeb3Container
       renderLoading={() => <div>Cargando la página. Aguarde por favor...</div>}
-      render={({ web3, accounts, contract, address }) => (
-        <Proyecto accounts={accounts} contract={contract} web3={web3} address={address} />
+      render={({ web3, accounts, contractDefinition }) => (
+        <Proyecto accounts={accounts} contractDefinition={contractDefinition} web3={web3} />
       )}
     />
   )
